503ce3c056c9bfdf88a42989f77811b26aa300a9
TuyenVuongSy <tuyenvs.ptit@gmail.com>
2025-05-28 21:47:21 +0700

    Commit file

---
+++ JS30/Day12_2025_05_28/content.js
@@ -0,0 +1,23 @@
+function updateFirstTitle() {
+    const firstTitle = document.querySelector("h3.article-title a");
+
+    if (firstTitle) {
+        firstTitle.textContent = "Sinh viên UDU ra trường có việc làm lương 1000$";
+        console.log("Đã thay tiêu đề đầu tiên");
+    } else {
+        console.log("Không tìm thấy tiêu đề đầu tiên");
+    }
+}
+
+const observer = new MutationObserver((mutations, obs) => {
+    const firstTitle = document.querySelector("h3.article-title a");
+    if (firstTitle) {
+        updateFirstTitle();
+        obs.disconnect();
+    }
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+});
